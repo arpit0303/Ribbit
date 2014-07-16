bfdeb1031a3bcc974142d30b137273a4590365f1
Arpit <arpitagrawal910@gmail.com>
2014-07-17 00:39:43 +0530

    first commit

---
+++ src/com/jaaga/ribbit/RibbitApplication.java
@@ -0,0 +1,20 @@
+package com.jaaga.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class RibbitApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "h9NQRJONNsqhl8sLOxPTdxnbG8dQknB3sAwrcT1W",
+				"tiiJKSPHf9HAoqdbx9lml8TSAzS7Cf9LkWKq9q6u");
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("dsjns", "sjbn");
+		testObject.saveInBackground();
+	}
+}
